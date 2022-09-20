5d5e41d71ce4ced63aef03d1446c8b6e637d4c7b
marshall-dieme <marshalldieme@gmail.com>
2022-09-20 20:40:09 +0000

    Environment set up

---
+++ passenger-details-service/src/test/java/com/spring/passengerdetailsservice/PassengerDetailsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.passengerdetailsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassengerDetailsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
